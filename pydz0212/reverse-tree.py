8b3c85b4d9f17611f880c43d63418a7a9cb13303
blackwhitebear <maxonleb5@gmail.com>
2022-04-28 22:40:30 +0300

    renamed

--- pydz0212/reverse-tree.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def invertTree(self, root):
-        if root is None:
-            return
-        root.right, root.left = root.left, root.right
-        self.invertTree(root.right)
-        self.invertTree(root.left)
-        return root
