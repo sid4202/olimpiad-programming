26bcb51ab7bbe5fe1ac75fe63400d893ad769819
Daniil <daniil@vivaldi.net>
2023-09-19 20:32:29 +0400

    Fix waffles

--- 46_2023-09-05/waffles.py
+++ 46_2023-09-05/waffles.py
@@ -7,7 +7,7 @@ for _ in range(t):
     n, m, d = list(map(int, input().split()))
     shop_cords = list(map(int, input().split()))
     shop_cords.append(1)
-    shop_cords.append(n)
+    shop_cords.append(n + 1)
 
     shop_cords.sort()
 
@@ -15,11 +15,8 @@ for _ in range(t):
     shops_to_delete = 0
 
     for i in range(1, m + 1):
-        print('A:', shop_cords[i - 1], 'B:', shop_cords[i], 'C:', shop_cords[i + 1])
         y2 = 1 + ((shop_cords[i + 1] - shop_cords[i - 1] - 1) // d)
         y1 = (shop_cords[i] - shop_cords[i - 1] - 1) // d + (shop_cords[i + 1] - shop_cords[i] - 1) // d + 2
-
-        print('y1:', y1, 'y2:', y2)
 
         if y2 < y1:
             shops_to_delete += 1
@@ -28,6 +25,7 @@ for _ in range(t):
             waffles_eaten += 2 + ((shop_cords[i] - shop_cords[i - 1] - 1) // d)
         else:
             waffles_eaten += 1 + ((shop_cords[i] - shop_cords[i - 1] - 1) // d)
+    waffles_eaten += ((shop_cords[-1] - shop_cords[-2] - 1) // d)
 
     if shops_to_delete:
         print(waffles_eaten - 1, shops_to_delete)
